12843aa9e847cd05e4823f67d67c1bbe0e98a0e5
Daniel Pradera <Daniel Pradera>
2016-03-17 12:09:01 +0100

    Se crean bolas hasta que se alcance el número introducido por el usuario.

--- BallDemo.java
+++ BallDemo.java
@@ -1,5 +1,6 @@
 import java.awt.Color;
-
+import java.util.ArrayList;
+import java.util.Random;
 /**
  * Class BallDemo - a short demonstration showing animation with the 
  * Canvas class. 
@@ -11,6 +12,7 @@ import java.awt.Color;
 public class BallDemo   
 {
     private Canvas myCanvas;
+    private ArrayList<BouncingBall> bolas;
 
     /**
      * Create a BallDemo object. Creates a fresh canvas and makes it visible.
@@ -18,12 +20,13 @@ public class BallDemo
     public BallDemo()
     {
         myCanvas = new Canvas("Ball Demo", 600, 500);
+        bolas = new ArrayList<BouncingBall>();
     }
 
     /**
      * Simulate two bouncing balls
      */
-    public void bounce()
+    public void bounce(int numBolas)
     {
         int ground = 400;   // position of the ground line
 
@@ -31,22 +34,32 @@ public class BallDemo
 
         // draw the ground
         myCanvas.drawLine(50, ground, 550, ground);
-
-        // crate and show the balls
-        BouncingBall ball = new BouncingBall(50, 50, 16, Color.BLUE, ground, myCanvas);
-        ball.draw();
-        BouncingBall ball2 = new BouncingBall(70, 80, 20, Color.RED, ground, myCanvas);
-        ball2.draw();
-
+        int x = 0;
+        int y = 0;
+        
+        // Crea bolas hasta q llega al número introducido por el usuario.
+        for (int cont = 0; cont <=numBolas; cont++)
+        {
+            // crate and show the balls
+            BouncingBall ball = new BouncingBall(x, y, 16, Color.BLUE, ground, myCanvas);
+            ball.draw();
+            bolas.add(ball);
+            x = x+5;
+            y = y+10;
+            cont++;
+        }
         // make them bounce
         boolean finished =  false;
+        int index = 0;
         while(!finished) {
-            myCanvas.wait(50);           // small delay
-            ball.move();
-            ball2.move();
-            // stop once ball has travelled a certain distance on x axis
-            if(ball.getXPosition() >= 550 || ball2.getXPosition() >= 550) {
-                finished = true;
+            for(BouncingBall ball : bolas)
+                {
+                myCanvas.wait(50);           // small delay
+                ball.move();
+                // stop once ball has travelled a certain distance on x axis
+                if(ball.getXPosition() >= 550) {
+                    finished = true;
+                }
             }
         }
     }
---
+++ README.txt.txt
@@ -0,0 +1,9 @@
+1.- Se crean bolas hasta que se alcance el número introducido por el usuario.
+
+2.- Posicion de aparición de las bolas aleatoria, entre la mitad y la izquierda de la pantalla.
+
+3.- El color de las bolas es aleatorio.
+
+4.- El radio de las bolas es aleatorio.
+
+5.- Si alguna bola se sale de la pantalla se detiene la ejecución.
